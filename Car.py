e015a854f69d616b30b6a747d5219e2fee6d9ba0
Jalal AlRukhaimi <conany1@gmail.com>
2019-03-16 11:49:48 +0300

    Rented default value is False

--- Car.py
+++ Car.py
@@ -1,10 +1,10 @@
 class Car:
-	def __init__(self, id, type, model, year, rented):
+	def __init__(self, id, type, model, year):
 		self.Id = id
 		self.Type = type
 		self.Model = model
 		self.Year = year
-		self.Rented = rented
+		self.Rented = False
 		
 	def rent(self):
 		self.Rented = True
