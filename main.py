99370e286cca65951aba51d8280792177f541a01
Jalal AlRukhaimi <conany1@gmail.com>
2019-03-22 09:55:53 +0300

    feat: Adding Car

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from Car Import car
+from Car import Car
 cars = []
-
+id = 0
 while True:
 	print("Enter 1 to Add a Car")
 	
 	s = input()
 	if (s == 1):
 		print("Enter car type")
+		t = raw_input()
+		print("Enter car model")
+		m = raw_input()
+		print("Enter car year")
+		y = input()
+		cars.append(Car(++id,t,m,y))
